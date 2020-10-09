86430a8c2629fd11d8e7556d376e4f65b0d6b7c1
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-10-09 14:49:57 +0800

    use go mod and add some function

--- aes/aes.go
+++ aes/aes.go
@@ -3,29 +3,50 @@ package AES
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"io/ioutil"
+
+	"github.com/liserjrqlxue/goUtil/simpleUtil"
 )
 
 // []byte length 16/24/32 ~ AES 128/192/256
 var commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
 
-func Encode(inputData []byte, codeKey []byte) ([]byte,error){
-	c,e:=aes.NewCipher(codeKey)
-	if e!=nil{
-		return nil,e
+func Encode(inputData []byte, codeKey []byte) ([]byte, error) {
+	c, e := aes.NewCipher(codeKey)
+	if e != nil {
+		return nil, e
 	}
 	cfb := cipher.NewCFBEncrypter(c, commonIV)
 	outputData := make([]byte, len(inputData))
 	cfb.XORKeyStream(outputData, inputData)
-	return outputData,e
+	return outputData, e
 }
 
-func Decode(inputData []byte, codeKey []byte) ([]byte,error){
-	c,e:=aes.NewCipher(codeKey)
-	if e!=nil{
-		return nil,e
+func Decode(inputData []byte, codeKey []byte) ([]byte, error) {
+	var c, e = aes.NewCipher(codeKey)
+	if e != nil {
+		return nil, e
 	}
-	cfbdec := cipher.NewCFBDecrypter(c, commonIV)
-	outputData := make([]byte, len(inputData))
+	var cfbdec = cipher.NewCFBDecrypter(c, commonIV)
+	var outputData = make([]byte, len(inputData))
 	cfbdec.XORKeyStream(outputData, inputData)
-	return outputData,e
+	return outputData, e
+}
+
+func DecodeFile(fileName string, codeKey []byte) []byte {
+	return simpleUtil.HandleError(
+		Decode(
+			simpleUtil.HandleError(ioutil.ReadFile(fileName)).([]byte),
+			codeKey,
+		),
+	).([]byte)
+}
+
+func EncodeFile(fileName string, codeKey []byte) []byte {
+	return simpleUtil.HandleError(
+		Encode(
+			simpleUtil.HandleError(ioutil.ReadFile(fileName)).([]byte),
+			codeKey,
+		),
+	).([]byte)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/liserjrqlxue/crypto
+
+go 1.13
+
+require github.com/liserjrqlxue/goUtil v0.0.30
